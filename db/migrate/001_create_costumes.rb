a9892bd8db15316af9726040e895d518b191c058
menhaj <menhaj007@yahoo.com>
2021-07-06 22:32:40 -0400

    commit tree 513668aaedee07d02d52c229e52e06975e67ca9a

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,28 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+# class CreateCostumes < ActiveRecord::Migration[4.2]
+#   def change
+#     create_table :costumes do |t|
+#     t.string :name
+#     t.float :price
+#     t.string :size
+#     t.string :image_url
+#     t.datetime :created_at
+#     t.datetime :update_at
+#   end
+#   end
+# end
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
+end
